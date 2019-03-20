1a890a483674c1039532af3ea6597c4416a38bd4
alilu8 <32311130+alilu8@users.noreply.github.com>
2019-03-20 18:30:51 +0100

    Add HelloWorld (Angel)

---
+++ src/Angel/PracticaDAA_HelloWorld.java
@@ -0,0 +1,22 @@
+
+package practicadaa;
+
+import java.util.Scanner;
+
+
+public class PracticaDAA_HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        for (int i=0;i<n;i++){
+            System.out.println("Hello World!");
+        }
+        
+    }
+    
+    
+}
